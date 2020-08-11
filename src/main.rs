42088338ea26fb08fa3ab00b833637e66b8db6e2
Vladislav Zhdanov <dev@null>
2020-08-11 20:40:17 +0300

    добавил содержимое функции постройки дерева зависимостей

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use petgraph::Graph;
+use petgraph::prelude::*;
 
 use std::env;
 
@@ -7,13 +7,44 @@ fn help() {
         emerge <string>");
 }
 
-fn build_dag(_packages: Vec<String>) -> Result<Graph::<&'static str, &'static str>, String> {
-    let deps = Graph::<&str, &str>::new();
+fn build_dag(packages_org: Vec<String>) -> Result<DiGraphMap::<&'static str, i8>, String> {
+    let mut graph = DiGraphMap::<&str, i8>::new();
+    let node_s = graph.add_node("s");
+    let node_t = graph.add_node("t");
 
-    Ok(deps)
+    let list = packages_org.to_vec();
+
+    while !list.is_empty() {
+        let package_name = list.pop();
+
+        parse_ebuild(&package_name)?;
+
+        if packages.contains(&package_name) {
+            check_restrictions()?;
+            merge_restrictions();
+        } else {
+            packages.add(&package_name);
+            graph.add_node(&package_name);
+        }
+
+        if packages_org.contains(&package_name) {
+            graph.add_edge(&package_name, &node_t, 1);
+        }
+        if package_depends.is_empty() {
+            graph.add_edge(&node_s, &package_name, 1);
+        }
+
+        list.extend_from_slice(package_depends.as_slice());
+    }
+
+    if !petgraph::algo::is_cyclic_directed(&graph) {
+        return Err("directed graph contains a cycle");
+    }
+
+    Ok(graph)
 }
 
-fn print_dag(dag: Graph::<&'static str, &'static str>) {
+fn print_dag(dag: DiGraphMap::<&'static str, i8>) {
     let order = petgraph::algo::toposort(&dag, None).unwrap();
     println!("{:?}", order);
 }
