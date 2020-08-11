75dbc9fabbcfcbf098da6f183a382e9183d653a3
Vladislav Zhdanov <dev@null>
2020-08-11 17:40:32 +0300

    добавил первоначальный каркас

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
+use std::env;
+
+fn help() {
+    println!();
+}
+
+fn build_dag(packages: Vec<String>) -> Result<(), String> {}
+
+fn print_dag() {
+    println!();
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() < 2 {
+        help();
+        return;
+    }
+
+    let result = build_dag(args[1..].to_vec());
+    if let Err(e) = result {
+        println!("error: {:?}", e);
+        return;
+    }
+
+    print_dag();
 }
