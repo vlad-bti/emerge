d0b1a2d0410ab8883f0539231d38945718bdaa06
Vladislav Zhdanov <dev@null>
2020-08-12 15:13:10 +0300

    поправил функцию постройки дерева зависимостей, сразу добавляются ноды и связи в дерево, информация о пакете добавляется позже (при чтении ебилда)

--- src/main.rs
+++ src/main.rs
@@ -7,34 +7,37 @@ fn help() {
         emerge <string>");
 }
 
-fn build_dag(packages_org: Vec<String>) -> Result<DiGraphMap::<&'static str, i8>, String> {
+fn build_dag(package_name_list: Vec<String>) -> Result<DiGraphMap::<&'static str, i8>, String> {
     let mut graph = DiGraphMap::<&str, i8>::new();
     let node_s = graph.add_node("s");
     let node_t = graph.add_node("t");
 
-    let list = packages_org.to_vec();
+    for package_name in package_name_list {
+        graph.add_node(&package_name);
+        graph.add_edge(&package_name, &node_t, 1);
+    }
 
-    while !list.is_empty() {
-        let package_name = list.pop();
+    while !package_name_list.is_empty() {
+        let package_name = package_name_list.pop();
 
-        parse_ebuild(&package_name)?;
+        package_info, package_depends_list = parse_ebuild(&package_name)?;
 
-        if packages.contains(&package_name) {
-            check_restrictions()?;
-            merge_restrictions();
-        } else {
-            packages.add(&package_name);
-            graph.add_node(&package_name);
-        }
-
-        if packages_org.contains(&package_name) {
-            graph.add_edge(&package_name, &node_t, 1);
-        }
-        if package_depends.is_empty() {
+        if package_depends_list.is_empty() {
             graph.add_edge(&node_s, &package_name, 1);
         }
 
-        list.extend_from_slice(package_depends.as_slice());
+        for depend_name in package_depends_list {
+            graph.add_node(&depend_name);
+            graph.add_edge(&depend_name, &package_name, 1);
+            package_name_list.push(depend_name);
+        }
+
+        if package_info_list.contains(&package_name) {
+            package_info_list.check_restrictions(package_info)?;
+            package_info_list.merge_restrictions(package_info);
+        } else {
+            package_info_list.push(package_info);
+        }
     }
 
     if !petgraph::algo::is_cyclic_directed(&graph) {
