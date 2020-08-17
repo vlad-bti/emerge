fb9c29d3a7ecbb12ec6ccce89dbd8fdefc79ef8f
Vladislav Zhdanov <vlad@yopart.ru>
2020-08-17 18:33:26 +0300

    добавил регексп для проверки корректности имени пакета

--- src/ebuild_utils.rs
+++ src/ebuild_utils.rs
@@ -19,9 +19,22 @@ fn load_ebuild(ebuild_name: String) {
     parse_uses()?;
 }
 
+// TODO: https://gitweb.gentoo.org/proj/portage.git/tree/lib/_emerge/is_valid_package_atom.py?h=portage-2.3.103
+//      https://gitweb.gentoo.org/proj/portage.git/tree/lib/portage/dep/__init__.py?h=portage-2.3.103
+//      https://gitweb.gentoo.org/proj/portage.git/tree/lib/portage/versions.py?h=portage-2.3.103
 fn parse_package_name(package_name: String) -> Result<PackageNameInfo, String> {
     lazy_static! {
-        static ref RE: Regex = Regex::new("...").unwrap();
+        let _slot = "([\w+][\w+.-]*)";
+        let _cat = "[\w+][\w+.-]*";
+        let _pkg = "[\w+][\w+-]*?";
+
+        let _v = "(\d+)((\.\d+)*)([a-z]?)((_(pre|p|beta|alpha|rc)\d*)*)";
+        let _rev = "\d+";
+        let _vr = format!("{}(-r({}))?", _v, _rev);
+
+        let _cp = format!("^({}/{}(-{})?(:{})?)$", _cat, _pkg, _vr, _slot);
+
+        static ref RE: Regex = Regex::new(_cp).unwrap();
     }
     if !RE.is_match(&package_name) {
         return Err(format!("'{}' is not a valid package atom.", package_name));
