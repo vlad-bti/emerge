51caea7f492ea00e90e5097771bd37499456c318
Vladislav Zhdanov <vlad@yopart.ru>
2020-08-17 15:04:48 +0300

    добавил модуль ebuild_utils.rs

---
+++ src/ebuild_utils.rs
@@ -0,0 +1,24 @@
+pub mod ebuild_utils {
+
+    fn load_ebuild(ebuild_name: String) {
+        parse_eapi()?;
+        parse_keywords()?;
+        parse_slot_subslot()?;
+        parse_depends()?;
+        parse_uses()?;
+    }
+
+    pub fn load_package_info(package_name: String) {
+        parse_package_name()?;
+        find_package()?;
+        let mut package_version_list = Vec::new();
+        if is_version_specify {
+            package_version_list.push(version);
+        } else {
+            package_version_list.append(version_list);
+        }
+        for version in package_version_list {
+            load_ebuild(verson)?;
+        }
+    }
+}
