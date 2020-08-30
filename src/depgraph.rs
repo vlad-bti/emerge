7f7c9e1c311a3fb7ac76c39168f69bf15a341dfa
Vladislav Zhdanov <vlad@yopart.ru>
2020-08-30 18:09:12 +0300

    обновил функцию построения дерева зависимостей, String -> &str

--- src/depgraph.rs
+++ src/depgraph.rs
@@ -17,34 +17,34 @@ pub fn init_dep_graph() -> GraphData {
 }
 
 pub trait DepGraph {
-    fn add_node(&mut self, node_name: String) -> Result<i32, String>;
-    fn add_edge(&mut self, node_name_a: String, node_name_b: String) -> Result<i8, String>;
+    fn add_node(&mut self, node_name: &str) -> Result<i32, String>;
+    fn add_edge(&mut self, node_name_a: &str, node_name_b: &str) -> Result<i8, String>;
     fn is_cyclic_directed(&self) -> bool;
     fn toposort(&self) -> Vec<String>;
 }
 
 impl DepGraph for GraphData {
-    fn add_node(&mut self, node_name: String) -> Result<i32, String> {
+    fn add_node(&mut self, node_name: &str) -> Result<i32, String> {
         let count = self.graph.node_count() as i32;
-        if self.name_to_index.contains_key(&node_name) {
+        if self.name_to_index.contains_key(node_name) {
             return Err(format!("'{}' node exists", node_name));
         }
-        self.name_to_index.insert(node_name.clone(), count);
-        self.index_to_name.insert(count, node_name);
+        self.name_to_index.insert(node_name.into(), count);
+        self.index_to_name.insert(count, node_name.into());
 
         Ok(self.graph.add_node(count))
     }
 
-    fn add_edge(&mut self, node_name_a: String, node_name_b: String) -> Result<i8, String> {
-        if !self.name_to_index.contains_key(&node_name_a) {
+    fn add_edge(&mut self, node_name_a: &str, node_name_b: &str) -> Result<i8, String> {
+        if !self.name_to_index.contains_key(node_name_a) {
             return Err(format!("'{}' node don't exists", node_name_a));
         }
-        if !self.name_to_index.contains_key(&node_name_b) {
+        if !self.name_to_index.contains_key(node_name_b) {
             return Err(format!("'{}' node don't exists", node_name_b));
         }
 
-        let index_a = self.name_to_index.get(&node_name_a).unwrap();
-        let index_b = self.name_to_index.get(&node_name_b).unwrap();
+        let index_a = self.name_to_index.get(node_name_a).unwrap();
+        let index_b = self.name_to_index.get(node_name_b).unwrap();
 
         Ok(self.graph.add_edge(*index_a, *index_b, 1).unwrap())
     }
--- src/main.rs
+++ src/main.rs
@@ -16,46 +16,44 @@ fn help() {
     );
 }
 
-fn build_dag(package_name_list: Vec<String>) -> Result<GraphData, String> {
-    let graph = init_dep_graph();
+fn build_dag(mut package_name_list: Vec<String>) -> Result<GraphData, String> {
+    let mut graph = init_dep_graph();
 
-    /*
+    let node_name_s = "s";
+    let node_name_t = "t";
 
-        let node_s = graph_data.graph.add_node(-1);
-        let node_t = graph_data.graph.add_node(-2);
+    graph.add_node(node_name_s)?;
+    graph.add_node(node_name_t)?;
 
-        let mut package_name_list: Vec<String> = Vec::new();
+    for package_name in package_name_list {
+        graph.add_node(&package_name);
+        graph.add_edge(&package_name, node_name_t);
+    }
 
-        for package_name in list {
-            graph.add_node(package_name.to_string().as_str());
-            graph.add_edge(package_name.as_str(), node_t, 1);
+    while !package_name_list.is_empty() {
+        let package_name = package_name_list.pop().unwrap();
+
+        let package_info = ebuild_utils::load_package_info(&package_name)?;
+
+        let package_version = package_info.version_list.first().unwrap();
+        if package_version.depends_list.is_empty() {
+            graph.add_edge(node_name_s, &package_name);
         }
 
-        while !package_name_list.is_empty() {
-            let package_name = package_name_list.pop().unwrap();
-
-            let package_info = ebuild_utils::load_package_info(package_name.as_str())?;
-
-            let package_version = package_info.version_list.first().unwrap();
-            if package_version.depends_list.is_empty() {
-                graph.add_edge(node_s, package_name.as_str(), 1);
-            }
-
-            for depend_name in package_version.depends_list.iter() {
-                graph.add_node(depend_name);
-                graph.add_edge(depend_name, package_name.as_str(), 1);
-                package_name_list.push(depend_name.into());
-            }
-            /*
-                    if package_info_list.contains(&package_name) {
-                        package_info_list.check_restrictions(&package_info)?;
-                        package_info_list.merge_restrictions(&package_info);
-                    } else {
-                        package_info_list.push(package_info);
-                    }
-            */
+        for depend_name in package_version.depends_list.iter() {
+            graph.add_node(depend_name);
+            graph.add_edge(depend_name, &package_name);
+            package_name_list.push(depend_name.into());
         }
-    */
+        /*
+                if package_info_list.contains(&package_name) {
+                    package_info_list.check_restrictions(&package_info)?;
+                    package_info_list.merge_restrictions(&package_info);
+                } else {
+                    package_info_list.push(package_info);
+                }
+        */
+    }
 
     if graph.is_cyclic_directed() {
         return Err(String::from("directed graph contains a cycle"));
